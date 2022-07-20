36d1e4786db4808938feb33e0ba77e724e29082e
Vladick <cool.valad12@gmail.com>
2022-07-20 18:26:15 +0300

    init commit

---
+++ src/main/java/com/idf/cryptowatcher/CryptowatcherApplication.java
@@ -0,0 +1,13 @@
+package com.idf.cryptowatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptowatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptowatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idf/cryptowatcher/CryptowatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idf.cryptowatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptowatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
