6a38836f677018e01fb45cebcb29a796e6e6df6f
Vladick <cool.valad12@gmail.com>
2022-07-21 08:59:28 +0300

    Update User dto

--- src/main/java/com/idf/cryptowatcher/dto/api/UserDto.java
+++ src/main/java/com/idf/cryptowatcher/dto/api/UserDto.java
@@ -1,4 +1,9 @@
 package com.idf.cryptowatcher.dto.api;
 
+import com.idf.cryptowatcher.domain.User;
+
+import java.util.Optional;
+
 public interface UserDto {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/idf/cryptowatcher/dto/impl/UserDtoImpl.java
+++ src/main/java/com/idf/cryptowatcher/dto/impl/UserDtoImpl.java
@@ -1,5 +1,18 @@
 package com.idf.cryptowatcher.dto.impl;
 
-public class UserDtoImpl {
+import com.idf.cryptowatcher.domain.User;
+import com.idf.cryptowatcher.dto.api.UserDto;
+import com.idf.cryptowatcher.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
+public class UserDtoImpl implements UserDto {
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
 }
