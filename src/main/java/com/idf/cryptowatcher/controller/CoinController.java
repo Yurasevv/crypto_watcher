bae3ea8fbf35dcd0789a24e4d94ecbeecd4e5dec
Vladick <cool.valad12@gmail.com>
2022-07-21 13:11:02 +0300

    Add coin controller

---
+++ src/main/java/com/idf/cryptowatcher/controller/CoinController.java
@@ -0,0 +1,31 @@
+package com.idf.cryptowatcher.controller;
+
+import com.idf.cryptowatcher.dto.CoinDto;
+import com.idf.cryptowatcher.service.CoinService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/coins")
+public class CoinController {
+
+    private CoinService coinService;
+
+    @GetMapping
+    public List<CoinDto> findAllCryptocurrency() {
+        return coinService.findAllCoins();
+    }
+
+    @GetMapping("/{symbol}")
+    public BigDecimal findPriceCryptocurrencyBySymbol(@PathVariable("symbol") String symbol) {
+        return coinService.findPriceBySymbol(symbol)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
